a5059967756ca594539cdf07a3684f9b6b69b936
emanoelcampos <emanoel.cv@gmail.com>
2022-10-25 01:20:18 -0300

    capturando erro

--- src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,8 +1,9 @@
-import { Observable } from 'rxjs';
+import { catchError, EMPTY, Observable, Subject } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
 
 import { Curso } from '../cursos';
 import { CursosService } from '../cursos.service';
+
 
 
 @Component({
@@ -16,6 +17,7 @@ export class CursosListaComponent implements OnInit {
   //cursos!: Curso[];
 
   cursos$!: Observable<Curso[]>;
+  error$ = new Subject<boolean>();
 
   constructor(private cursosService: CursosService) {}
 
@@ -23,7 +25,14 @@ export class CursosListaComponent implements OnInit {
     // this.cursosService.list().subscribe(
     //   dados => this.cursos = dados);
 
-    this.cursos$ = this.cursosService.list();
+    this.cursos$ = this.cursosService.list()
+    .pipe(
+      catchError(error => {
+        console.log(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    );
   }
 
 }
