b6120da04a76574ea1bd9bae078da0871a3d7e9a
emanoelcampos <emanoel.cv@gmail.com>
2022-11-23 01:15:50 -0300

    Upload de Arquivo: Request com FormData

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,3 +1,4 @@
+import { UploadFileService } from './../upload-file.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadFileComponent implements OnInit {
 
-  constructor() { }
+  files!: Set<File>;
+
+  constructor(private uploadService: UploadFileService) { }
 
   ngOnInit(): void {
   }
@@ -20,10 +23,19 @@ export class UploadFileComponent implements OnInit {
     //document.getElementById('formFile')!.innerHTML = selectedFiles[0].name;
 
     const fileNames = [];
+    this.files = new Set();
     for (let i=0; i<selectedFiles.length; i++) {
       fileNames.push(selectedFiles[i].name);
+      this.files.add(selectedFiles[i]);
     }
     document.getElementById('formFile')!.innerHTML = fileNames.join(', ');
+  }
+
+  onUpload() {
+    if (this.files && this.files.size > 0) {
+      this.uploadService.upload(this.files, 'http://localhost:8000/upload')
+      .subscribe(response => console.log('upload concluído'));
+    }
   }
 
 }
